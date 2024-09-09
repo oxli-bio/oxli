c3e54f695c9e2ae5478e7a443e53faa872107a92
Adam Taranto <adam.p.taranto@gmail.com>
2024-09-09 15:31:59 +1000

    Update tests

--- src/python/tests/test_basic.py
+++ src/python/tests/test_basic.py
@@ -1,6 +1,8 @@
-import pytest
 import oxli
+import pytest
 
+
+# Helper function, create tables.
 def create_sample_kmer_table(ksize, kmers):
     table = oxli.KmerCountTable(ksize)
     for kmer in kmers:
@@ -8,7 +10,8 @@ def create_sample_kmer_table(ksize, kmers):
     return table
 
 
-def test_simple():
+# Adding Kmers
+def test_count():
     # yo dawg it works
     cg = oxli.KmerCountTable(4)
     kmer = "ATCG"
@@ -16,6 +19,24 @@ def test_simple():
     assert cg.get(kmer) == 0
     assert cg.count(kmer) == 1
     assert cg.get(kmer) == 1
+
+
+def test_count_hash():
+    kmer = "TAAACCCTAACCCTAACCCTAACCCTAACCC"
+    cg = oxli.KmerCountTable(ksize=31)
+    hashkey = cg.hash_kmer(kmer)
+
+    assert cg.get_hash(hashkey) == 0
+    assert cg.count_hash(hashkey) == 1
+    assert cg.get_hash(hashkey) == 1
+
+
+def test_hash_rc():
+    table = create_sample_kmer_table(3, ["AAA", "TTT", "AAC"])
+    hash_aaa = table.hash_kmer("AAA")  # 10679328328772601858
+    hash_ttt = table.hash_kmer("TTT")  # 10679328328772601858
+
+    assert hash_aaa == hash_ttt, "Hash should be same for reverse complement."
 
 
 def test_wrong_ksize():
@@ -47,15 +68,14 @@ def test_consume_2():
     assert cg.consume(seq) == 2
     assert cg.get("ATCG") == 1
     assert cg.get("TCGG") == 1
-    assert cg.get("CCGA") == 1 # reverse complement!
+    assert cg.get("CCGA") == 1  # reverse complement!
 
 
 def test_consume_bad_DNA():
     # test an invalid base in last position
     cg = oxli.KmerCountTable(4)
     seq = "ATCGGX"
-    with pytest.raises(ValueError,
-                        match="bad k-mer encountered at position 2"):
+    with pytest.raises(ValueError, match="bad k-mer encountered at position 2"):
         cg.consume(seq, allow_bad_kmers=False)
 
 
@@ -63,8 +83,7 @@ def test_consume_bad_DNA_2():
     # test an invalid base in first position
     cg = oxli.KmerCountTable(4)
     seq = "XATCGG"
-    with pytest.raises(ValueError,
-                        match="bad k-mer encountered at position 0"):
+    with pytest.raises(ValueError, match="bad k-mer encountered at position 0"):
         cg.consume(seq, allow_bad_kmers=False)
 
 
@@ -75,7 +94,7 @@ def test_consume_bad_DNA_ignore():
     print(cg.consume(seq, allow_bad_kmers=True))
     assert cg.get("ATCG") == 1
     assert cg.get("TCGG") == 1
-    assert cg.get("CCGA") == 1 # rc
+    assert cg.get("CCGA") == 1  # rc
 
 
 def test_consume_bad_DNA_ignore_is_default():
@@ -85,104 +104,151 @@ def test_consume_bad_DNA_ignore_is_default():
     print(cg.consume(seq))
     assert cg.get("ATCG") == 1
     assert cg.get("TCGG") == 1
-    assert cg.get("CCGA") == 1 # rc
+    assert cg.get("CCGA") == 1  # rc
 
 
-def test_count_get():
+# Test attributes
+def test_hashes_attribute():
+    table = create_sample_kmer_table(3, ["AAA", "TTT", "AAC"])
+    hashes = table.hashes
+    hash_aaa = table.hash_kmer("AAA")  # 10679328328772601858
+    hash_ttt = table.hash_kmer("TTT")  # 10679328328772601858
+    hash_aac = table.hash_kmer("AAC")  # 6579496673972597301
+
+    expected_hashes = set(
+        [hash_aaa, hash_ttt, hash_aac]
+    )  # {10679328328772601858, 6579496673972597301}
+    assert (
+        set(hashes) == expected_hashes
+    ), ".hashes attribute should match the expected set of hash keys"
+
+
+# Getting counts
+def test_count_vs_counthash():
     # test a bug reported by adam taranto: count and get should work together!
-    kmer = 'TAAACCCTAACCCTAACCCTAACCCTAACCC'
-
+    kmer = "TAAACCCTAACCCTAACCCTAACCCTAACCC"
     cg = oxli.KmerCountTable(ksize=31)
     hashkey = cg.hash_kmer(kmer)
 
     assert cg.get(kmer) == 0
     assert cg.count(kmer) == 1
     assert cg.count(kmer) == 2
+    assert cg.get(kmer) == 2
+    assert cg.count_hash(hashkey) == 3
 
     x = cg.get(kmer)
-    assert x == 2, x
-    
+    assert x == 3, x
+
 
 def test_get_hash():
-    ''' Retrieve counts using hash key. '''
-    table = create_sample_kmer_table(3, ['AAA', 'TTT', 'AAC'])
+    """Retrieve counts using hash key."""
+    table = create_sample_kmer_table(3, ["AAA", "TTT", "AAC"])
     # Find hash of kmer 'AAA'
-    hash_aaa = table.hash_kmer('AAA') # 10679328328772601858
+    hash_aaa = table.hash_kmer("AAA")  # 10679328328772601858
     # Lookup counts for hash of 'AAA' and rc 'TTT'
     count_aaa = table.get_hash(hash_aaa)
-    
     assert count_aaa == 2, "Hash count for 'AAA' should be 2"
 
-    hash_aac = table.hash_kmer('AAC') # 6579496673972597301
+    # Test single kmer
+    hash_aac = table.hash_kmer("AAC")  # 6579496673972597301
     count_aac = table.get_hash(hash_aac)
-    
     assert count_aac == 1, "Hash count for 'AAC' should be 1"
 
-def test_get_hash_array():
-    ''' 
-    Get vector of counts corresponding to vector of hash keys.
-    '''
-    table = create_sample_kmer_table(3, ['AAA', 'AAC'])
-    hash_aaa = table.hash_kmer('AAA')
-    hash_aac = table.hash_kmer('AAC')
-    hash_ggg = table.hash_kmer('GGG') # key not in table
-    
-    hash_keys = [hash_aaa, hash_aac, hash_ggg]
-    counts = table.get_hash_array(hash_keys)
-    
-    assert counts == [1, 1, 0], "Hash array counts should match the counts of 'AAA' and 'AAC' and return zero for 'GGG'."
+    # Test for kmer that is not in table
+    hash_aag = table.hash_kmer("AAG")  # 12774992397053849803
+    count_aag = table.get_hash(hash_aag)
+    assert count_aag == 0, "Missing kmer count for 'AAG' should be 0"
 
+
+def test_get_hash_array():
+    """
+    Get vector of counts corresponding to vector of hash keys.
+    """
+    table = create_sample_kmer_table(3, ["AAA", "TTT", "AAC"])
+    hash_aaa = table.hash_kmer("AAA")
+    hash_aac = table.hash_kmer("AAC")
+    hash_ggg = table.hash_kmer("GGG")  # key not in table
+
+    hash_keys = [hash_aaa, hash_aac, hash_ggg]
+    hash_keys_rev = [hash_ggg, hash_aac, hash_aaa]
+
+    counts = table.get_hash_array(hash_keys)
+    rev_counts = table.get_hash_array(hash_keys_rev)
+
+    assert (
+        counts == [2, 1, 0]
+    ), "Hash array counts should match the counts of 'AAA' and 'AAC' and return zero for 'GGG'."
+    assert rev_counts == [0, 1, 2], "Count should be in same order as input list"
+
+
+def test_get_array():
+    """
+    Get vector of counts corresponding to vector of kmers.
+    """
+    # TODO: Add function to get list of counts given list of kmers.
+    pass
+
+
+# Set operations
 def test_union():
-    table1 = create_sample_kmer_table(3, ['AAA', 'AAC'])
-    table2 = create_sample_kmer_table(3, ['AAC', 'AAG'])
-    
+    table1 = create_sample_kmer_table(3, ["AAA", "AAC"])
+    table2 = create_sample_kmer_table(3, ["AAC", "AAG"])
+
     union_set = table1.union(table2)
     expected_union = set(table1.hashes).union(table2.hashes)
-    
+
     assert union_set == expected_union, "Union of hash sets should match"
 
+
 def test_intersection():
-    table1 = create_sample_kmer_table(3, ['AAA', 'AAC'])
-    table2 = create_sample_kmer_table(3, ['AAC', 'AAG'])
-    
+    table1 = create_sample_kmer_table(3, ["AAA", "AAC"])
+    table2 = create_sample_kmer_table(3, ["AAC", "AAG"])
+
     intersection_set = table1.intersection(table2)
     expected_intersection = set(table1.hashes).intersection(table2.hashes)
-    
-    assert intersection_set == expected_intersection, "Intersection of hash sets should match"
+
+    assert (
+        intersection_set == expected_intersection
+    ), "Intersection of hash sets should match"
+
 
 def test_difference():
-    table1 = create_sample_kmer_table(3, ['AAA', 'AAC'])
-    table2 = create_sample_kmer_table(3, ['AAC', 'AAG'])
-    
+    table1 = create_sample_kmer_table(3, ["AAA", "AAC"])
+    table2 = create_sample_kmer_table(3, ["AAC", "AAG"])
+
     difference_set = table1.difference(table2)
     expected_difference = set(table1.hashes).difference(table2.hashes)
-    
+
     assert difference_set == expected_difference, "Difference of hash sets should match"
 
+
 def test_symmetric_difference():
-    table1 = create_sample_kmer_table(3, ['AAA', 'AAC'])
-    table2 = create_sample_kmer_table(3, ['AAC', 'AAG'])
-    
+    table1 = create_sample_kmer_table(3, ["AAA", "AAC"])
+    table2 = create_sample_kmer_table(3, ["AAC", "AAG"])
+
     symmetric_difference_set = table1.symmetric_difference(table2)
-    expected_symmetric_difference = set(table1.hashes).symmetric_difference(table2.hashes)
-    
-    assert symmetric_difference_set == expected_symmetric_difference, "Symmetric difference of hash sets should match"
+    expected_symmetric_difference = set(table1.hashes).symmetric_difference(
+        table2.hashes
+    )
+
+    assert (
+        symmetric_difference_set == expected_symmetric_difference
+    ), "Symmetric difference of hash sets should match"
+
 
 def test_dunder_methods():
-    table1 = create_sample_kmer_table(3, ['AAA', 'AAC'])
-    table2 = create_sample_kmer_table(3, ['AAC', 'AAG'])
-    
-    assert table1.__or__(table2) == table1.union(table2), "__or__ method should match union()"
-    assert table1.__and__(table2) == table1.intersection(table2), "__and__ method should match intersection()"
-    assert table1.__sub__(table2) == table1.difference(table2), "__sub__ method should match difference()"
-    assert table1.__xor__(table2) == table1.symmetric_difference(table2), "__xor__ method should match symmetric_difference()"
+    table1 = create_sample_kmer_table(3, ["AAA", "AAC"])
+    table2 = create_sample_kmer_table(3, ["AAC", "AAG"])
 
-def test_hashes_attribute():
-    table = create_sample_kmer_table(3, ['AAA', 'TTT', 'AAC'])
-    hashes = table.hashes
-    hash_aaa = table.hash_kmer('AAA') #10679328328772601858
-    hash_ttt = table.hash_kmer('TTT') #10679328328772601858
-    hash_aac = table.hash_kmer('AAC') #6579496673972597301
-    
-    expected_hashes = set([hash_aaa, hash_ttt, hash_aac]) #{10679328328772601858, 6579496673972597301}
-    assert set(hashes) == expected_hashes, ".hashes attribute should match the expected set of hash keys"
+    assert table1.__or__(table2) == table1.union(
+        table2
+    ), "__or__ method should match union()"
+    assert table1.__and__(table2) == table1.intersection(
+        table2
+    ), "__and__ method should match intersection()"
+    assert table1.__sub__(table2) == table1.difference(
+        table2
+    ), "__sub__ method should match difference()"
+    assert table1.__xor__(table2) == table1.symmetric_difference(
+        table2
+    ), "__xor__ method should match symmetric_difference()"
