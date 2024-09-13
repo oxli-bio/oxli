5448ccfc5bf427964d0f195884433e7a42a3b59c
Adam Taranto <adam.p.taranto@gmail.com>
2024-09-13 15:29:28 +1000

    Add Set Operation Methods (#12)
    * ignore jupyter checkpoints
    
    * Add basic set operations
    
    * fix typo
    
    * Reduce hashmap lookup by one in count_hash by using entry() API
    
    * Get counts by hash or by list of hashes
    
    * Add .hashes attribute
    
    * Tidy up imports
    
    * Add tests for setops and get counts by hash
    
    * cargo fmt
    
    * Update tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-use pyo3::exceptions::PyValueError;
-use pyo3::prelude::*;
-// use rayon::prelude::*;
+// Standard library imports
+use std::collections::{HashMap, HashSet};
 
+// External crate imports
 use anyhow::{anyhow, Result};
 use log::debug;
-use std::collections::HashMap;
-
-// use sourmash::sketch::nodegraph::Nodegraph;
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
 use sourmash::encodings::HashFunctions;
 use sourmash::signature::SeqToHashes;
 
@@ -47,14 +46,9 @@ impl KmerCountTable {
     }
 
     pub fn count_hash(&mut self, hashval: u64) -> u64 {
-        let mut count: u64 = 1;
-        if self.counts.contains_key(&hashval) {
-            count = *self.counts.get(&hashval).unwrap();
-            count = count + 1;
-        }
-        self.counts.insert(hashval, count);
-
-        count
+        let count = self.counts.entry(hashval).or_insert(0);
+        *count += 1;
+        *count
     }
 
     pub fn count(&mut self, kmer: String) -> PyResult<u64> {
@@ -86,7 +80,26 @@ impl KmerCountTable {
         }
     }
 
-    // Consume this DNA strnig. Return number of k-mers consumed.
+    // Get the count for a specific hash value directly
+    pub fn get_hash(&self, hashval: u64) -> u64 {
+        // Return the count for the hash value, or 0 if it does not exist
+        *self.counts.get(&hashval).unwrap_or(&0)
+    }
+
+    // Get counts for a list of hash keys and return an list of counts
+    pub fn get_hash_array(&self, hash_keys: Vec<u64>) -> Vec<u64> {
+        // Map each hash key to its count, defaulting to 0 if the key is not present
+        hash_keys.iter().map(|&key| self.get_hash(key)).collect()
+    }
+
+    // Getter for the 'hashes' attribute, returning all hash keys in the table
+    #[getter]
+    pub fn hashes(&self) -> Vec<u64> {
+        // Collect and return all keys from the counts HashMap
+        self.counts.keys().cloned().collect()
+    }
+
+    // Consume this DNA string. Return number of k-mers consumed.
     #[pyo3(signature = (seq, allow_bad_kmers=true))]
     pub fn consume(&mut self, seq: String, allow_bad_kmers: bool) -> PyResult<u64> {
         let hashes = SeqToHashes::new(
@@ -118,8 +131,57 @@ impl KmerCountTable {
 
         Ok(n)
     }
+
+    // Helper method to get hash set of k-mers
+    fn hash_set(&self) -> HashSet<u64> {
+        self.counts.keys().cloned().collect()
+    }
+
+    // Set operation methods
+    pub fn union(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.hash_set().union(&other.hash_set()).cloned().collect()
+    }
+
+    pub fn intersection(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.hash_set()
+            .intersection(&other.hash_set())
+            .cloned()
+            .collect()
+    }
+
+    pub fn difference(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.hash_set()
+            .difference(&other.hash_set())
+            .cloned()
+            .collect()
+    }
+
+    pub fn symmetric_difference(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.hash_set()
+            .symmetric_difference(&other.hash_set())
+            .cloned()
+            .collect()
+    }
+
+    // Python dunder methods for set operations
+    fn __or__(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.union(other)
+    }
+
+    fn __and__(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.intersection(other)
+    }
+
+    fn __sub__(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.difference(other)
+    }
+
+    fn __xor__(&self, other: &KmerCountTable) -> HashSet<u64> {
+        self.symmetric_difference(other)
+    }
 }
 
+// Python module definition
 #[pymodule]
 fn oxli(m: &Bound<'_, PyModule>) -> PyResult<()> {
     env_logger::init();
