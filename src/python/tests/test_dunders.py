06e148be419fa89d116136d3c315d26a3e96b5f2
Adam Taranto <adam.p.taranto@gmail.com>
2024-09-14 22:18:38 +1000

    Add tests for __len__ method.

--- src/python/tests/test_dunders.py
+++ src/python/tests/test_dunders.py
@@ -3,23 +3,43 @@ import pytest
 from test_basic import create_sample_kmer_table
 
 
-def test_len_dunder_method():
-    '''__len__ should return number of keys in KmerCountTable.'''
-    pass
+# Test __len__
+def test_len_initial():
+    kmer_table = oxli.KmerCountTable(ksize=16)
+    assert len(kmer_table) == 0, "Initial length should be 0"
+
+
+def test_len_after_count():
+    kmer_table = oxli.KmerCountTable(ksize=16)
+    kmer_table.count("ACGTACGTACGTACGT")  # Adds 1 unique k-mer
+    assert len(kmer_table) == 1, "Length should be 1 after adding one unique k-mer"
+
+
+def test_len_after_multiple_counts():
+    kmer_table = oxli.KmerCountTable(ksize=16)
+    kmer_table.count("ACGTACGTACGTACGT")  # Adds 1 unique k-mer
+    kmer_table.count("ACGTACGTACGTACGT")  # Adds 1 repeat k-mer
+    kmer_table.count("CCCCCCCCCCCCCCCC")  # Adds 1 unique k-mer
+    kmer_table.consume("GCTAGCTAGCTA")  # Adds 0 k-mers
+    assert len(kmer_table) == 2, "Length should be 2 after adding two unique k-mers"
+
 
 def test_iter_dunder_method():
-    '''KmerCountTable should be iterable, yield hash:count pairs'''
+    """KmerCountTable should be iterable, yield hash:count pairs"""
     pass
+
 
 def test_next_dunder_method():
-    '''Select next key in generator'''
+    """Select next key in generator"""
     pass
 
+
 def test_getitem_dunder_method():
-    '''Query an object to using the indexing syntax (obj[key])'''
+    """Query an object to using the indexing syntax (obj[key])"""
     # Same behaviour as .get()
     pass
 
+
 def test_setitem_dunder_method():
-    '''Set values using the indexing syntax (obj[key] = value)'''
+    """Set values using the indexing syntax (obj[key] = value)"""
     pass
