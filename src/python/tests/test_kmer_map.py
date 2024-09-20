8f354c118494a5ad5e6ca32cb0b7895336f24a45
Adamtaranto <Adamtaranto@users.noreply.github.com>
2024-09-20 04:04:33 +0000

    Style fixes by Ruff

--- src/python/tests/test_kmer_map.py
+++ src/python/tests/test_kmer_map.py
@@ -4,5 +4,5 @@ from test_basic import create_sample_kmer_table
 
 
 def test_kmermap():
-    '''Test option to add kmermap'''
+    """Test option to add kmermap"""
     pass
--- src/python/tests/test_output.py
+++ src/python/tests/test_output.py
@@ -4,21 +4,25 @@ from test_basic import create_sample_kmer_table
 
 
 def test_serialise():
-    '''Serialise object to JSON '''
+    """Serialise object to JSON"""
     pass
+
 
 def test_deserialise():
-    '''Load object from file.'''
+    """Load object from file."""
     pass
+
 
 def test_dump():
-    '''Write tab delimited kmer:count pairs'''
+    """Write tab delimited kmer:count pairs"""
     pass
+
 
 def test_dump_hash():
-    '''Write tab delimited hash_count pairs '''
+    """Write tab delimited hash_count pairs"""
     pass
 
+
 def test_histo():
-    '''Write frequency counts.'''
+    """Write frequency counts."""
     pass
